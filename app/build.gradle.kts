ea73bfc1beb597f2460c52eb2a402e692f7138f0
Pixelh97 <abakhet2010@gmail.com>
2025-09-03 17:00:00 +0300

    Downgrade Koin to v3.5.6 and remove deprecated ViewModel dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,7 +61,6 @@ dependencies {
     // Koin
     implementation(libs.koin.android)
     implementation(libs.koin.core)
-    implementation(libs.koin.androidx.viewmodel)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
